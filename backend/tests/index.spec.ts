bf87f41798d99d3014f47309357b0ae1b21b728e
Sonia Volpe <sonia.volpe@frvr.com>
2025-09-23 09:35:12 +0200

    fix: test

--- backend/tests/index.spec.ts
+++ backend/tests/index.spec.ts
@@ -2,7 +2,4 @@ describe('Simple Addition Test', () => {
   it('should add two numbers correctly', () => {
     expect(1 + 2).toBe(3);
   });
-  it('should fail', () => {
-    expect(1 + 2).toBe(4);
-  });
 });
